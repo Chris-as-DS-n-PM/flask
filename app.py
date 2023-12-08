34e12c2ccac5b2b0da1e3290baa06001b35bac73
SirotovAB <35331316+SirotovAB@users.noreply.github.com>
2023-12-09 00:52:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/_about')
+def about():
+    return Response("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><html>about</html>", content_type='text/xml')
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/cgi-bin/systemutil.cgi', methods=['POST'])
+def system_util():
+    params = request.form
+    code = "2" if params.get("user") == "admin" and params.get("password") == "admin" else "0"
+    response_content = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" + \
+                       "<xml>\n" + \
+                       "<Login>{}</Login>\n".format(code) + \
+                       "</xml>"
+    return Response(response_content, content_type='text/xml')
+
+@app.route('/cgi-bin/modem.cgi', methods=['GET'])
+def modem_cgi():
+    command = request.args.get("Command")
+    if command == "getAntenna":
+        response_content = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" + \
+                           "<xml>\n" + \
+                           "<antenna_value>2</antenna_value>\n" + \
+                           "</xml>\n"
+    elif command == "getModemStatus":
+        response_content = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" + \
+                           "<xml>\n" + \
+                           "<antenna_value>2</antenna_value>\n" + \
+                           "</xml>\n"
+    else:
+        return "Invalid command", 400
+
+    return Response(response_content, content_type='text/xml')
