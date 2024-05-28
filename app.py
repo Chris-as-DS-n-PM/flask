114524c72a285b82895529fb7330e23e8f21e7eb
Aymeric Noblanc <46248176+AymericNoblanc@users.noreply.github.com>
2024-05-28 15:26:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
-url = os.environ.get("postgres://test_jpgf_user:RCYokXSELN3K7Tjtr249F5lh6qz4Pnga@dpg-cpasrdtds78s73d7k3ag-a/test_jpgf")  # gets variables from environment
+url = os.environ.get("postgres://test_jpgf_user:RCYokXSELN3K7Tjtr249F5lh6qz4Pnga@dpg-cpasrdtds78s73d7k3ag-a.frankfurt-postgres.render.com/test_jpgf")  # gets variables from environment
 connection = psycopg2.connect(url)
 
 SELECT = """SELECT * FROM test;"""
