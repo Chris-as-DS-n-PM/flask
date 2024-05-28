40c4ac82511c6198b01bb19ece743360dc353ee5
Aymeric Noblanc <46248176+AymericNoblanc@users.noreply.github.com>
2024-05-28 15:45:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
 import os
 import psycopg2
-#from dotenv import load_dotenv
-
-#load_dotenv()  # loads variables from .env file into environment
 
 from flask import Flask
 app = Flask(__name__)
 
-url = os.environ.get("postgres://test_jpgf_user:RCYokXSELN3K7Tjtr249F5lh6qz4Pnga@dpg-cpasrdtds78s73d7k3ag-a.frankfurt-postgres.render.com/test_jpgf")  # gets variables from environment
-connection = psycopg2.connect(url)
+conn = psycopg2.connect(dbname='test_jpgf', user='test_jpgf_user', password='RCYokXSELN3K7Tjtr249F5lh6qz4Pnga', host='dpg-cpasrdtds78s73d7k3ag-a.frankfurt-postgres.render.com', port='5432', sslmode='require')
 
 SELECT = """SELECT * FROM test;"""
 
 @app.route('/')
 def hello_world():
-    with connection:
-        with connection.cursor() as cursor:
+    with conn:
+        with conn.cursor() as cursor:
             cursor.execute(SELECT)
             average = cursor.fetchone()[0]
-    return 'Hello, World!' + average
+    return "Hello World " + str(average)
