72cfff1b8fc0aa1ed414364af2ef0a4e3c4cb278
jxtdav3 <akeredolud@mercia.school>
2025-09-12 09:56:25 +0100

    commit

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask("Studsight")
+app.secret_key = "davidneastudsightkey.com"
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Welcome to studsight!'
+
+@app.route("/login") # Login route
+def login():
+    pass
+
+@app.route("/callback") # Callback route
+def callback():
+    pass#
+
+@app.route("/logout") # Logout route
+def logout():
+    pass
+
+@app.route("/") # Home route
+def index():
+    return render_template("index.html")
+
+@app.route("/protected_area") 
+def protected_area():
+    pass
+
+if __name__ == "__main__": # Run the app
+    app.run(debug=True)
