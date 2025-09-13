4dc70f0a5bc5a93df29d1d357d6642a43a9e9d7f
jxtdav3 <akeredolud@mercia.school>
2025-09-13 05:36:11 +0000

    fin2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, abort, redirect, url_for, request
 import pathlib,os
-from googl.oauth2 import id_token
+from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from pip._vendor import cachecontrol
 import google.auth.transport.requests
