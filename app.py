fe7bcb927930934aa9dd86739d97b0e0c20917ef
ralfwaldhauer-cmd <ralf.waldhauer@googlemail.com>
2025-10-10 10:22:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/api/berlin-data")
+def berlin_data():
+    data = [
+        {
+            "category": "Fußballverein",
+            "name": "FC Viktoria 1889 Berlin",
+            "email": "vereinsverwaltung@viktoria.berlin",
+            "district": "Tempelhof-Schöneberg",
+            "source": "https://viktoria.berlin"
+        },
+        {
+            "category": "Kita",
+            "name": "INA.KINDER.GARTEN Prenzlauer Berg",
+            "email": "prenzlauerberg@inakindergarten.de",
+            "district": "Pankow",
+            "source": "https://inakindergarten.de"
+        },
+        {
+            "category": "Private Schule",
+            "name": "Berlin Metropolitan School",
+            "email": "info@metropolitanschool.com",
+            "district": "Mitte",
+            "source": "https://metropolitanschool.com"
+        }
+    ]
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
